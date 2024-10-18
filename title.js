9a965d442ce0cce171fc59b1f6968211ad5a5855
Versiya v1.0.1 <versiya.v1.0.1@gmail.com>
2024-10-18 10:18:42 +0500

    update title.js

--- title.js
+++ title.js
@@ -1,4 +1,4 @@
-const title = ('Uralbek UZ');
-document.getElementById('title').innerHTML = title;
 const title = ('&#9776');
 document.getElementById('menu').innerHTML = title;
+const title = ('Uralbek UZ');
+document.getElementById('title').innerHTML = title;
